e617593a108905298e6b1e468ef7733f686ba944
CdoubleO <lautarocoo@gmail.com>
2020-07-23 02:46:40 +0000

    add classes to rollhelpernew.py

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import discord
 import random
+from rollhelpernew import Roll
 
 client = discord.Client()
 servers = {}
 
 basic_help_message = '```Dungeon Keep Help:\n\n\t- Purge all bot-related messages: !purge\n\t- Roll dices: !roll\n\n- For details on more complex commands, type !help command (eg. !help roll)```'
-roll_help_message = '```Dungeon Keep Roll Command Help:\n\n\tAliases: !roll 1d20 #alias_name creates an alias. Afterwards, !roll #alias_name will roll 1d20\n\tTHIS FEATURE IS IN DEVELOPMENT! OH NOES! COME BACK LATER :D```'
+roll_help_message = '```Dungeon Keep Roll Command Help:\n\n\tRepetition:!roll 2 2d6 + 3d10 repeats two times a roll of two six-sided dice and three ten-sided dice. If there is no number the default is one repetition Aliases: !roll 1d20 #alias_name creates an alias. Afterwards, !roll #alias_name will roll 1d20\n\tTHIS FEATURE IS IN DEVELOPMENT! OH NOES! COME BACK LATER :D```'
 
 @client.event
 async def on_ready():
@@ -15,7 +16,7 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
+    message.content = message.content.lower()
     if message.content.startswith('!roll '):
         await process_roll(message)
     elif message.content.startswith('!purge'):
@@ -58,10 +59,19 @@ async def process_roll(message):
                 await message.channel.send('Could not find alias %s' % command[1:])
                 return
             else:
-                await message.channel.send('Rolling %s: %s' % (command[1:], servers[message.guild.id][command[1:]])) 
+                await message.channel.send('Rolling %s (%s)' % (command[1:], servers[message.guild.id][command[1:]])) 
+                command = servers[message.guild.id][command[1:]]
         else:   
             [command,alias] = [seg.strip() for seg in command.split('#')]
             servers[message.guild.id][alias] = command
             await message.channel.send('Saved alias %s: %s' % (command,alias))
+    
+
+    cmd = Roll(command)
+    result = cmd.getResult()
+    await message.channel.send('%s rolled %s:' % (message.author.display_name,command))
+    for arr in result:
+        flattened = [item for sublist in arr for item in sublist]
+        await message.channel.send('%s: %s' % (flattened,sum(flattened)))
 
 client.run('')
---
+++ rollhelpernew.py
@@ -0,0 +1,43 @@
+import random
+
+class Roll():
+    def __init__(self, command):
+        i = 1
+        if ' ' in command:
+            idx = command.index(' ')
+        if 'd' not in command[:idx]:
+            i = int(command[:idx])
+            command = command[idx+1:].replace(' ', '')
+
+        self.repetition = i
+        self.dice = [Die(die) for die in command.split('+')]
+
+
+    def getResult(self):
+        results = []
+        for _ in range(self.repetition):
+            currResult = []
+            for die in self.dice:
+                currResult.append(die.rollDie())
+            results.append(currResult)
+        return results
+
+
+class Die():
+    def __init__(self,die):
+        self.die = die
+        if 'd' in self.die: 
+            idx = self.die.index('d')
+            self.num_times = int(self.die[:idx])
+            self.die_type = int(self.die[idx+1:])
+            self.isFixed = False
+        else:
+            self.num_times = 1
+            self.die_type = 1
+            self.isFixed = True
+
+    def rollDie(self):
+        if self.isFixed:
+            return [int(self.die)]
+        return [random.randint(1,self.die_type) for _ in range(self.num_times)]
+    
