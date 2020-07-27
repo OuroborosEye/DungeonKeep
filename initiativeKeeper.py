f8b27deaa0e976b7f7ef4f711875f11052c1907d
CdoubleO <68615325+CdoubleO@users.noreply.github.com>
2020-07-27 20:08:12 -0300

    Fixed loss of !Init next (#3)
    * restore init next feature
    
    * Fix issue: Init Next must be relative to the guild_id

--- initiativeKeeper.py
+++ initiativeKeeper.py
@@ -1,7 +1,7 @@
 
 class initiativeKeeper():
     def __init__(self):
-        self.guild_initiatives = {}
+        self.guild_initiatives = {}   
 
     def runCommand(self,message):
         command = message.content.lower().replace('!init ', '')
@@ -9,35 +9,48 @@ class initiativeKeeper():
         guild_id = message.guild.id
     
         if guild_id not in self.guild_initiatives:
-            self.guild_initiatives[guild_id] = {} 
-
+            self.guild_initiatives[guild_id] = {'Initiatives':{}, 'Init_Index':-1} 
         if command.startswith('clear'):
             return  self.clearInitiatives(guild_id)
         elif command.startswith('show'):
             return self.printInitiatives(guild_id,message.guild.name)
         elif command.startswith('add'):
             return self.addInitiative(guild_id, command.replace('add ',''))
+        elif command.startswith('next'):
+            return self.nextInitiative(guild_id)
         else:
             return 'Invalid command'
 
     def addInitiative(self,guild_id,command):
         try:
             char,init = command.split(' ')
-            self.guild_initiatives[guild_id][char] = init
+            self.guild_initiatives[guild_id]['Initiatives'][char] = init
+            self.guild_initiatives[guild_id]['Init_Index'] = 0
             return 'Initiative Added'
         except:
             return 'Invalid initiatives to add'
 
     def clearInitiatives(self,guild_id):
-        self.guild_initiatives[guild_id] = {}
+        self.guild_initiatives[guild_id] = {'Initiatives':{}, 'Init_Index':-1}
         return 'Initiatives cleared'
 
     def printInitiatives(self,guild_id,guild_name):
-        if len(self.guild_initiatives[guild_id]) == 0:
+        if len(self.guild_initiatives[guild_id]['Initiatives']) == 0:
             return 'No initiatives saved'
 
         output = '```Initiatives for %s\n' % guild_name
-        for row in sorted(self.guild_initiatives[guild_id].items(),key=lambda k:k[1],reverse=True):
+        for row in sorted(self.guild_initiatives[guild_id]['Initiatives'].items(), reverse=True, key=lambda k:int(k[1])):
             output += '%s: %s\n' % (row[0],row[1])
         output += '```'
+        return output
+    
+    def nextInitiative(self,guild_id):
+        idx = self.guild_initiatives[guild_id]['Init_Index']
+        if len(self.guild_initiatives[guild_id]['Initiatives']) == 0:
+            return 'No initiatives saved'
+        output = '```Next turn is for: %s```' % (sorted(self.guild_initiatives[guild_id]['Initiatives'].items(), reverse=True, key=lambda k: int(k[1]))[idx][0])
+        idx += 1
+        if idx == len(self.guild_initiatives[guild_id]['Initiatives']):
+            idx = 0
+        self.guild_initiatives[guild_id]['Init_Index'] = idx
         return output
